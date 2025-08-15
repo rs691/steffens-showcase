4f8d406eff07f86ce59cac2826d3d94d443aed32
rs691 <robstewart@my365.bellevue.edu>
2025-08-15 14:24:36 -0500

    fix base path

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Required for static site on GitHub Pages
-  basePath: '/steffens-showcase', // Repo name
+  basePath: '/steffens-showcase/', // Repo name
   assetPrefix: '/steffens-showcase/',
   images: {
     unoptimized: true // Disable image optimization
