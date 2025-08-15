ba786962e048fae43f1eab8edc574f40a76a8277
rs691 <robstewart@my365.bellevue.edu>
2025-08-15 14:58:52 -0500

    fix base again

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Required for static site on GitHub Pages
-    site: 'https://rs691.github.io/',
   basePath: '/steffens-showcase', // Repo name
   assetPrefix: '/steffens-showcase/',
   images: {
