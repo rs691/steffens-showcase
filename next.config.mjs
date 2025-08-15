82eabfc4c7903e3adbecf31aa21a5baa1be87219
rs691 <robstewart@my365.bellevue.edu>
2025-08-15 14:29:27 -0500

    file path 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // Required for static site on GitHub Pages
-  basePath: '/rs691.github.io/steffens-showcase', // Repo name
-  assetPrefix: '/rs691.github.io/steffens-showcase/',
+  basePath: '/steffens-showcase', // Repo name
+  assetPrefix: '/steffens-showcase/',
   images: {
     unoptimized: true // Disable image optimization
   }
