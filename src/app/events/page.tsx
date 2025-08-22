13a8c8e264657f3c5456b7d17a46d5aa2088e58b
rs691 <robstewart@my365.bellevue.edu>
2025-08-21 23:09:27 -0500

    added event to Event's page

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -3,10 +3,10 @@ import PageContainer from '@/components/ui/PageContainer';
 import type { EventInfo } from '@/types';
 
 const events: EventInfo[] = [
-  { id: '1', name: 'Fall Festival', date: 'Oct 4 - Oct 5, 2025, 10:00 AM - 12:00 AM', location: 'Florence, Omaha, USA', description: 'Join us for the annual Fall Festival! Discover unique handmade items from local artisans, including our latest woodworking creations.', imageUrl: '/steffens-showcase/fallFestEvent.jpg' },
-  { id: '2', name: 'Summer Market Days', date: 'Dec 13, 2025, 9:00 AM - 6:00 PM', location: 'Aksarben Village, Omaha, USA', description: 'Find unique handcrafted goods all weekend long. We\'ll have a booth showcasing our custom signs, furniture, and home decor.', imageUrl: '/steffens-showcase/jingleMingle.jpg' },
-  { id: '3', name: 'Fall Festival of Arts', date: 'October 5, 2025, 10:00 AM - 2:00 PM', location: 'Maple Street, Old Town District', description: 'Learn wood basics with our expert artisans.', imageUrl: '/steffens-showcase/woodClassPoster1.png' },
-  { id: '4', name: 'Holiday Craft Show', date: 'December 7-8, 2025, 12:00 PM - 4:00 PM', location: 'Community Center Auditorium', description: 'Learn hand carving and techniques for creating beautiful wooden ornaments.', imageUrl: '/steffens-showcase/woodClassPoster2.png' },
+  { id: '1', name: 'Fall Festival', date: 'Oct 4 - Oct 5, 2025, 10:00 AM - 12:00 AM', location: 'Florence, Omaha, NE', description: 'Join us for the annual Fall Festival! Discover unique handmade items from local artisans, including our latest woodworking creations.', imageUrl: '/steffens-showcase/fallFestEvent.jpg' },
+  { id: '2', name: 'Summer Market Days', date: 'Dec 13, 2025, 9:00 AM - 6:00 PM', location: 'Aksarben Village, Omaha, NE', description: 'Find unique handcrafted goods all weekend long. We\'ll have a booth showcasing our custom signs, furniture, and home decor.', imageUrl: '/steffens-showcase/jingleMingle.jpg' },
+  { id: '3', name: '54th Annual Rockbrook Village Art Fair', date: 'September 6-7, 2025, 10:00 AM - 2:00 PM', location: '2800 S 110th Street, Omaha, NE', description: 'Come see some of the best local artists and crafters at this annual event.', imageUrl: '/steffens-showcase/rockVill.svg' },
+  { id: '4', name: 'Holiday Craft Show', date: 'December 7-8, 2025, 12:00 PM - 4:00 PM', location: 'Community Center Auditorium, Omaha, NE', description: 'Learn hand carving and techniques for creating beautiful wooden ornaments.', imageUrl: '/steffens-showcase/woodClassPoster2.png' },
 ];
 
 export default function EventsPage() {
